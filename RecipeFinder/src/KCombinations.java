5b2ccfa9b1744ff6863cc9318a0d03b018e63c0c
'Benjamin <'bsmith00oo@yahoo.com'>
2019-09-20 19:03:21 -0400

    Combinations

--- RecipeFinder/src/KCombinations.java
+++ RecipeFinder/src/KCombinations.java
@@ -1,9 +1,18 @@
+import java.util.List;
 
 public class KCombinations {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public void helper(List<int[]> combinations, int data[], int start, int end, int index) {
+	    if (index == data.length) {
+	        int[] combination = data.clone();
+	        combinations.add(combination);
+	    } else {
+	        int max = Math.min(end, end + 1 - data.length + index);
+	        for (int i = start; i <= max; i++) {
+	            data[index] = i;
+	            helper(combinations, data, i + 1, end, index + 1);
+	        }
+	    }
 	}
 
 }
